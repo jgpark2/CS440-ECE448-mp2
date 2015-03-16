9db88ac7320b79857f55b41e2dda5dbd84dba414
Jonathan Park <jgpark2@illinois.edu>
2015-03-15 19:21:27 -0500

    Added prereq list for fwd tracking in course

--- course.h
+++ course.h
@@ -2,6 +2,7 @@
 #define COURSE_H
 
 #include <vector>
+#include <map>
 
 /*
  * Course class, these courses have unique IDs that can be mapped 1-to-1 to array indices.
@@ -28,6 +29,9 @@ class Course
 		int credit;
 		int semesterID;
 		std::vector <Course*> prereqList;
+		
+		//map of semesterID's (value domain) to bool (assignable or no longer possible)
+		std::map <int, bool> reaminingValues;
 };
 
 #endif
